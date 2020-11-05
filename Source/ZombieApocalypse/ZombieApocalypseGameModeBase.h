b2e018a9aaa22021e9d9f3739c141d522ca7acff
M.Abdullah Sultan <abdullahsultan448@gmail.com>
2020-11-05 16:47:33 +0500

    Initial comit with some work

---
+++ Source/ZombieApocalypse/ZombieApocalypseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZombieApocalypseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZOMBIEAPOCALYPSE_API AZombieApocalypseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
